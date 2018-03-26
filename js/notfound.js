f838933b9e38fe6d957f45314041a023555fa8cf
karifan <karifan.stap@gmail.com>
2018-03-26 20:41:56 +0300

    not found page

--- js/notfound.js
+++ js/notfound.js
@@ -1,1 +1,51 @@
-webpackJsonp([0],[,,,,,function(n,o,c){"use strict";c(6),c(7),console.log(404)},function(n,o){},function(n,o){}],[5]);
+jQuery(function($){
+
+	var $loader = $('#loader');
+
+	var gp = getPath();
+	var st = 11;
+
+
+	$loader.find('.link')
+		.text(gp)
+		.attr('href',gp)
+		.end()
+		.find('.time')
+		.text(st)
+		.end()
+		.find('.load')
+		.addClass('progress')
+
+	setInterval(function(){
+		st--;
+		if (st < 0) st = 0;
+		$loader.find('.time').text(st)
+	},1000)
+
+	setTimeout(function(){
+		$loader.find('.load')
+			.removeClass('progress')
+			.addClass('redirect');
+
+		setTimeout(function(){
+			location.href = gp;
+		},500)
+	},11000)
+
+})
+
+var getPath = function() {
+	return ('' + location.origin + '/#' + location.pathname + location.search)
+}
+
+
+
+var rotate = function($r,timer, d) {
+	$r.css('width',Math.round(Math.max(d.width,d.height) * 0.95))
+	$r.css('height',Math.round(Math.max(d.width,d.height) * 0.95))
+}
+
+var loader = function($l, timer) {
+
+}
+
