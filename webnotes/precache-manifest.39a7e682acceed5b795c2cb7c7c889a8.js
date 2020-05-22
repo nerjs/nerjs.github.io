e43d7cdbce6127ad2b3a17434133c2b234175671
nerjs <nerjs.stap@gmail.com>
2020-05-22 12:13:08 +0000

    notes_bs: Rebuild from dispatch

---
+++ webnotes/precache-manifest.39a7e682acceed5b795c2cb7c7c889a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00fe9f56ab2ad38036edb6f202283446",
+    "url": "/webnotes/index.html"
+  },
+  {
+    "revision": "67b5670185e4569c80fe",
+    "url": "/webnotes/static/js/2.da5101ab.chunk.js"
+  },
+  {
+    "revision": "3ef063d808f83919fbf6e033e62c20cf",
+    "url": "/webnotes/static/js/2.da5101ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad2b8e7f0fafc72e2271",
+    "url": "/webnotes/static/js/main.cefb1ce3.chunk.js"
+  },
+  {
+    "revision": "c80a6707ce1bfe54d8b7",
+    "url": "/webnotes/static/js/runtime-main.031d7152.js"
+  }
+]);
